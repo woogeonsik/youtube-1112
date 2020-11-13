b654b1cd1f74e789849ecab073ab559917cff67b
woogeonsik1 <68350491+woogeonsik1@users.noreply.github.com>
2020-11-13 22:47:09 +0900

    user Profile 출력

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,6 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
-  console.log(req.user);
   next();
 };
 
